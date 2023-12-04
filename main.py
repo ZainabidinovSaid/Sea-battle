388f7b84986be8c7e1df5ec6776b46713b073973
unknown <saidislam1233@gmail.com>
2023-12-04 12:00:15 +0600

    4 commit

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ class Player:
         self.board = [[' ' for _ in range(7)] for _ in range(7)]
         self.ships = [Ship(3), Ship(2), Ship(2), Ship(1), Ship(1), Ship(1), Ship(1)]
         self.shots = set()        
+
+def print_board(board):
+    print("   A B C D E F G")
+    print("  ----------------")
+    for i in range(7):
+        print(f"{i + 1}|", end=" ")
+        for j in range(7):
+            print(board[i][j], end=" ")
+        print()        
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')        
