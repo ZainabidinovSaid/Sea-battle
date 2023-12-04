c6c8b6bc8b9a483b4b4b9fdd776c811a724d691f
unknown <saidislam1233@gmail.com>
2023-12-04 21:58:06 +0600

    7 commit

--- main.py
+++ main.py
@@ -31,7 +31,47 @@ def place_ship(board, ship):
         def is_clear(x, y):
             if 0 <= x < 7 and 0 <= y < 7 and board[y][x] == 'O':
                 return True
-            return False             
+            return False 
+
+        def is_clear_around(x, y):
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if 0 <= x + i < 7 and 0 <= y + j < 7:
+                        if board[y + j][x + i] != 'O':
+                            return False
+            return True
+
+        if orientation == 'horizontal':
+            for i in range(size):
+                if not is_clear(x + i, y) or not is_clear_around(x + i, y):
+                    return False
+        else:  # Vertical
+            for i in range(size):
+                if not is_clear(x, y + i) or not is_clear_around(x, y + i):
+                    return False
+
+        return True
+
+    for size, symbol in ships:
+        while True:
+            orientation = random.choice(['horizontal', 'vertical'])
+            if orientation == 'horizontal':
+                x = random.randint(0, 6 - size)
+                y = random.randint(0, 6)
+            else:
+                x = random.randint(0, 6)
+                y = random.randint(0, 6 - size)
+
+            if is_valid_placement(x, y, size, orientation, board):
+                for i in range(size):
+                    if orientation == 'horizontal':
+                        board[y][x + i] = symbol
+                    else:
+                        board[y + i][x] = symbol
+                break
+
+    return board
+            
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')        
