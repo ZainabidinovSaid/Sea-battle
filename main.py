1b989f025809d1d19c7c88fb1b6beaf2366fcd65
unknown <saidislam1233@gmail.com>
2023-12-04 11:06:39 +0600

    third commit

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ class Ship:
     def __init__(self, size):
         self.size = size
         self.hits = 0
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.board = [[' ' for _ in range(7)] for _ in range(7)]
+        self.ships = [Ship(3), Ship(2), Ship(2), Ship(1), Ship(1), Ship(1), Ship(1)]
+        self.shots = set()        
